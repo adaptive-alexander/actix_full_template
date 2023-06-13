9b525e5df68c4b593b5ea4d5c057dd01aafcadc0
Alexander Hyll <alexander.hyll@lumera.com>
2023-06-13 13:56:16 +0200

    Fix, minor

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 mod logging;
 
-use env_logger::Env;
-use tracing::info;
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Args {
 }
 
 fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     dotenvy::dotenv().expect("Failed to read .env file");
 
-    info!("Parsing arguments");
+    tracing::info!("Parsing arguments");
     let _args = Args::parse();
 }
