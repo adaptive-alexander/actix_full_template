4e834e5460ed4495cfcedc2845fd393983989621
Alexander Hyll <alexander.hyll@lumera.com>
2023-06-13 14:47:11 +0200

    Added tracing functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@ mod tests;
 mod error;
 mod config;
 mod prelude;
+pub mod logging;
 
 pub fn sync_test(_inp: &str){}
 
+#[tracing::instrument(
+    name = "Async test subscriber",
+)]
 pub async fn async_test(_inp: usize){}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-mod logging;
+use template::logging::{get_subscriber,init_subscriber};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about=None)]
-struct Args {
-}
+#[command(author, version, about, long_about = None)]
+struct Args {}
 
 fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("template".into(), "info".into());
+    init_subscriber(subscriber);
 
     dotenvy::dotenv().expect("Failed to read .env file");
 
-    tracing::info!("Parsing arguments");
+    tracing::info!("Parsing args");
+
     let _args = Args::parse();
 }
