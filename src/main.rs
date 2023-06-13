4e81bda9f01d2085a7fb1efab3d5f1908a426f0a
Alexander Hyll <alexander.hyll@lumera.com>
2023-06-13 11:55:41 +0200

    Add tracing and dotenvy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 mod logging;
 
+use env_logger::Env;
+use tracing::info;
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Args {
 }
 
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    dotenvy::dotenv().expect("Failed to read .env file");
+
+    info!("Parsing arguments");
     let _args = Args::parse();
 }
