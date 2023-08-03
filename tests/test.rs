1fab13837d13faa48c980bc4b659d614d08446d4
Alexander Hyll <alexander.hyll@lumera.com>
2023-08-03 10:35:52 +0200

    Add first integration test

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,23 @@
-use template::sync_test;
+fn spawn_app() {
+    let server = template::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future
+    let _ = tokio::spawn(server);
+}
 
-#[test]
-fn bb_test() {
-    sync_test("argument")
+#[tokio::test]
+async fn health_check_works() {
+    // Runs server as a background task
+    spawn_app().await.expect("Failed to spawn our app.");
+
+    // Perform HTTP requests against our application.
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
